f6bd43f6cfef6f19fe76610b3d00a871fa6adf3f
nemanja-gajicic <nemanja.gajicic@gmail.com>
2022-12-28 21:50:24 +0100

    Updating the ApiService
    Adding the missing methods such as searching by name, ingredient

--- src/app/_service/api.service.ts
+++ src/app/_service/api.service.ts
@@ -7,47 +7,74 @@ import { Drink } from '../_models/drink.model';
   providedIn: 'root',
 })
 export class ApiService {
-    // search
-    // lookup
-    // list
-    // filter
-    constructor(private httpClient: HttpClient) { }
-    searchCocktailByFirstLetter(firstLetter: string) {
-        return this.httpClient
-                        .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + firstLetter)
-                        
-           
-    }
+  // search
+  // lookup
+  // list
+  // filter
+  constructor(private httpClient: HttpClient) { }
 
-    lookupDrinkById(id: string) {
-      return this.httpClient
-        .get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id)
-        .pipe( map( (response: any) => {
-          
-          const drink: Drink = response.drinks[0] as Drink;
-          drink.ingredients = [];
-          drink.instructions = [];
-          Object.keys(drink).forEach( (key) => {
-            const keyName = key as keyof typeof drink;
-            
-            if(key.startsWith('strIngredient') && drink[keyName]) {
-              const index = key.replace('strIngredient', '');
-              const keyMeasure = 'strMeasure' + index as keyof typeof drink;
-              console.log(index);
-              drink.ingredients.push({
-                name: drink[keyName],
-                measure: drink[keyMeasure]
-              });
+
+  searchCocktailByFirstLetter(firstLetter: string) {
+    return this.httpClient
+      .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?f=' + firstLetter)
+  }
+
+  searchCocktailById(id: string){
+    return this.httpClient
+    .get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id)
+  }
+
+  searchCocktailByName(name: string) {
+    return this.httpClient
+    .get('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + name)
+  }
+
+  searchCocktailByIngredient(ingredient: string) {
+    return this.httpClient
+    .get('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=' + ingredient)
+  }
+
+  searchRandomCocktail() {
+    return this.httpClient
+    .get('https://www.thecocktaildb.com/api/json/v1/1/random.php');
+  }
+
+  searchIngredientsList() { 
+    return this.httpClient
+    .get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+  }
+  
+  lookupDrinkById(id: string) {
+    return this.httpClient
+      .get('https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=' + id)
+      .pipe(map((response: any) => {
+
+        const drink: Drink = response.drinks[0] as Drink;
+        drink.ingredients = [];
+        drink.instructions = [];
+        Object.keys(drink).forEach((key) => {
+          const keyName = key as keyof typeof drink;
+
+          if (key.startsWith('strIngredient') && drink[keyName]) {
+            const index = key.replace('strIngredient', '');
+            const keyMeasure = 'strMeasure' + index as keyof typeof drink;
+            console.log(index);
+            drink.ingredients.push({
+              name: drink[keyName],
+              measure: drink[keyMeasure]
+            });
+          }
+          if (key.startsWith('strInstructions') && drink[keyName]) {
+            let lang = key.replace('strInstructions', '');
+            if (!lang) {
+              lang = 'EN';
             }
-            if(key.startsWith('strInstructions') && drink[keyName]) {
-              let lang = key.replace('strInstructions', '');
-              if (!lang) {
-                lang = 'EN';
-              }
-              drink.instructions[lang] = drink[keyName]
-            }
-          });
-          return drink;
-        }))
-    }
+            drink.instructions[lang] = drink[keyName]
+          }
+        });
+        return drink;
+      }))
+  }
+
+
 }
